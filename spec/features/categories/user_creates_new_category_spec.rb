06f22077bcde16605771183fa20e57184219cf21
Chris Smith <42@chrisryansmith.com>
2017-05-18 14:50:13 -0600

    initial create category spec

---
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.feature "User creates a new category" do
+  context "with valid params" do
+    scenario "a user can create a new category" do
+
+    end
+
+  end
+end
