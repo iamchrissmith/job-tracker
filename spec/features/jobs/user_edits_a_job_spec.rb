cd95ea48ee69a20a46f5f7bb4996426d97849159
Chris Smith <42@chrisryansmith.com>
2017-05-18 14:15:16 -0600

    add sad paths and error display for edit

--- spec/features/jobs/user_edits_a_job_spec.rb
+++ spec/features/jobs/user_edits_a_job_spec.rb
@@ -1,49 +1,86 @@
 require 'rails_helper'
 
 describe "User edits an existing job" do
-  scenario "a user can edit a job title" do
-    job = create(:job)
-    company = job.company
-    company = job.company
-    visit edit_company_job_path(company, job)
+  context "with valid params" do
+    scenario "a user can edit a job title" do
+      job = create(:job)
+      company = job.company
+      company = job.company
+      visit edit_company_job_path(company, job)
 
-    fill_in "job[title]", with: "New Job Title"
-    click_button "Update Job"
-    job.reload
+      fill_in "job[title]", with: "New Job Title"
+      click_button "Update Job"
+      job.reload
 
-    expect(current_path).to eq(company_job_path(company, job))
-    expect(page).to have_content "#{job.title} updated!"
-    expect(page).to have_content "New Job Title"
-    expect(page).not_to have_content "Developer"
+      expect(current_path).to eq(company_job_path(company, job))
+      expect(page).to have_content "#{job.title} updated!"
+      expect(page).to have_content "New Job Title"
+      expect(page).not_to have_content "Developer"
+    end
+
+    scenario "a user can edit a job level_of_interest" do
+      job = create(:job)
+      company = job.company
+      visit edit_company_job_path(company, job)
+
+      fill_in "job[level_of_interest]", with: 30
+      click_button "Update Job"
+      job.reload
+
+      expect(current_path).to eq(company_job_path(company, job))
+      expect(page).to have_content "#{job.title} updated!"
+      expect(page).to have_content 30
+      expect(page).not_to have_content 70
+    end
+
+    scenario "a user can edit a job city" do
+      job = create(:job)
+      company = job.company
+      visit edit_company_job_path(company, job)
+
+      fill_in "job[city]", with: "Casper"
+      click_button "Update Job"
+      job.reload
+
+      expect(current_path).to eq(company_job_path(company, job))
+      expect(page).to have_content "#{job.title} updated!"
+      expect(page).to have_content "Casper"
+      expect(page).not_to have_content "Denver"
+    end
   end
+  context "with invalid params" do
+    scenario "a user can edit a job title" do
+      job = create(:job)
+      company = job.company
+      company = job.company
+      visit edit_company_job_path(company, job)
 
-  scenario "a user can edit a job level_of_interest" do
-    job = create(:job)
-    company = job.company
-    visit edit_company_job_path(company, job)
+      fill_in "job[title]", with: ""
+      click_button "Update Job"
 
-    fill_in "job[level_of_interest]", with: 30
-    click_button "Update Job"
-    job.reload
+      expect(page).to have_content "Title can't be blank"
+    end
 
-    expect(current_path).to eq(company_job_path(company, job))
-    expect(page).to have_content "#{job.title} updated!"
-    expect(page).to have_content 30
-    expect(page).not_to have_content 70
-  end
+    scenario "a user can edit a job level_of_interest" do
+      job = create(:job)
+      company = job.company
+      visit edit_company_job_path(company, job)
 
-  scenario "a user can edit a job city" do
-    job = create(:job)
-    company = job.company
-    visit edit_company_job_path(company, job)
+      fill_in "job[level_of_interest]", with: ''
+      click_button "Update Job"
 
-    fill_in "job[city]", with: "Casper"
-    click_button "Update Job"
-    job.reload
+      expect(page).to have_content "Level of interest can't be blank"
+    end
 
-    expect(current_path).to eq(company_job_path(company, job))
-    expect(page).to have_content "#{job.title} updated!"
-    expect(page).to have_content "Casper"
-    expect(page).not_to have_content "Denver"
+    scenario "a user can edit a job city" do
+      job = create(:job)
+      company = job.company
+      visit edit_company_job_path(company, job)
+
+      fill_in "job[city]", with: ""
+      click_button "Update Job"
+
+      expect(page).to have_content "City can't be blank"
+    end
   end
 end
