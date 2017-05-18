17fc8b3f8ad83a24da029fd47124357ea6c02a87
Chris Smith <42@chrisryansmith.com>
2017-05-18 14:10:06 -0600

    happy paths working for job edit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,16 +1,15 @@
 class JobsController < ApplicationController
+  before_action :set_company
+  before_action :set_job, only: [:show, :edit, :update]
   def index
-    @company = Company.find(params[:company_id])
     @jobs = @company.jobs
   end
 
   def new
-    @company = Company.find(params[:company_id])
     @job = Job.new()
   end
 
   def create
-    @company = Company.find(params[:company_id])
     @job = @company.jobs.new(job_params)
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}"
@@ -21,15 +20,19 @@ class JobsController < ApplicationController
   end
 
   def show
-    @job = Job.find(params[:id])
   end
 
   def edit
-    # implement on your own!
   end
 
   def update
-    # implement on your own!
+    @job.update(job_params)
+    if @job.save
+      flash[:success] = "#{@job.title} updated!"
+      redirect_to company_job_path(@company, @job)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -37,6 +40,14 @@ class JobsController < ApplicationController
   end
 
   private
+
+   def set_company
+     @company = Company.find(params[:company_id])
+   end
+
+   def set_job
+     @job = Job.find(params[:id])
+   end
 
   def job_params
     params.require(:job).permit(:title, :description, :level_of_interest, :city)
--- spec/features/jobs/user_edits_a_job_spec.rb
+++ spec/features/jobs/user_edits_a_job_spec.rb
@@ -3,36 +3,46 @@ require 'rails_helper'
 describe "User edits an existing job" do
   scenario "a user can edit a job title" do
     job = create(:job)
-    visit edit_job_path(job)
+    company = job.company
+    company = job.company
+    visit edit_company_job_path(company, job)
 
     fill_in "job[title]", with: "New Job Title"
     click_button "Update Job"
+    job.reload
 
-    expect(current_path).to eq(job_path(job))
+    expect(current_path).to eq(company_job_path(company, job))
+    expect(page).to have_content "#{job.title} updated!"
     expect(page).to have_content "New Job Title"
     expect(page).not_to have_content "Developer"
   end
 
   scenario "a user can edit a job level_of_interest" do
     job = create(:job)
-    visit edit_job_path(job)
+    company = job.company
+    visit edit_company_job_path(company, job)
 
     fill_in "job[level_of_interest]", with: 30
     click_button "Update Job"
+    job.reload
 
-    expect(current_path).to eq(job_path(job))
+    expect(current_path).to eq(company_job_path(company, job))
+    expect(page).to have_content "#{job.title} updated!"
     expect(page).to have_content 30
     expect(page).not_to have_content 70
   end
 
   scenario "a user can edit a job city" do
     job = create(:job)
-    visit edit_job_path(job)
+    company = job.company
+    visit edit_company_job_path(company, job)
 
     fill_in "job[city]", with: "Casper"
     click_button "Update Job"
+    job.reload
 
-    expect(current_path).to eq(job_path(job))
+    expect(current_path).to eq(company_job_path(company, job))
+    expect(page).to have_content "#{job.title} updated!"
     expect(page).to have_content "Casper"
     expect(page).not_to have_content "Denver"
   end
