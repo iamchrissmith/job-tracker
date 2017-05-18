30cb29dc9e8c032b7e5e6a810dceaf78b593a6db
Chris Smith <42@chrisryansmith.com>
2017-05-18 15:05:20 -0600

    add category create and spec

--- spec/features/categories/user_creates_new_category_spec.rb
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -3,8 +3,15 @@ require 'rails_helper'
 RSpec.feature "User creates a new category" do
   context "with valid params" do
     scenario "a user can create a new category" do
+      category_title = "Web Development"
+      visit new_category_path
 
+      fill_in "category[title]", with: category_title
+
+      click_button "Create Category"
+
+      expect(current_path).to eq category_path(Category.first)
+      expect(page).to have_content category_title
     end
-
   end
 end
