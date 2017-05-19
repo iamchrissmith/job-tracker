bc3088417e5eb368596b5439bc74df43c50b8ceb
Chris Smith <42@chrisryansmith.com>
2017-05-19 10:48:12 -0600

    add category select to job form and spec

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -3,12 +3,16 @@ require 'rails_helper'
 describe "User creates a new job" do
   scenario "a user can create a new job" do
     company = create(:company)
+    category = create(:category)
     visit new_company_job_path(company)
 
     fill_in "job[title]", with: "Developer"
     fill_in "job[description]", with: "So fun!"
     fill_in "job[level_of_interest]", with: 80
     fill_in "job[city]", with: "Denver"
+
+    select category.title, :from => "job[category_id]"
+    expect(page).to have_link "Add new category", new_category_path
 
     click_button "Create"
 
