e0a8664934ab933dfa96f9bd477d3c0700ddc970
Chris Smith <42@chrisryansmith.com>
2017-05-20 20:40:06 -0600

    pass model spec for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
+  validates :author, presence: true
+  validates :comment, presence: true
   belongs_to :job
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   context "validations" do
-    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:author) }
     it { is_expected.to validate_presence_of(:comment) }
   end
 end
