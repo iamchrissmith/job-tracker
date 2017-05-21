9870f64f6e9b43c9a82b8af2ff90be9f7cc2e79b
Chris Smith <42@chrisryansmith.com>
2017-05-20 20:27:27 -0600

    add comments factory

---
+++ spec/factories/comments.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :comment do
+    author "John Doe"
+    comment "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras faucibus rutrum urna, vel tincidunt elit lacinia eget. Donec magna lectus, vulputate eget varius cursus, condimentum nec erat. Duis facilisis leo nibh, at fermentum purus. Donec dignissim neque id velit scelerisque id rhoncus est malesuada. Pellentesque elit risus, faucibus vel vulputate eget, malesuada eu tortor. Donec vulputate pretium odio, id congue velit consectetur in. Fusce lobortis ultrices elementum. Suspendisse potenti."
+    job
+  end
+end
--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -5,5 +5,15 @@ FactoryGirl.define do
     title "Developer"
     level_of_interest 70
     city "Denver"
+
+    factory :job_with_comments do
+      transient do
+        comments_count 3
+      end
+
+      after(:create) do |job, evaluator|
+        create_list(:comment, evaluator.comments_count, job: job)
+      end
+    end
   end
 end
