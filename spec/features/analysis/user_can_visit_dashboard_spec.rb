0f10fc50f67a3d6fac31b74437051b093d507082
Chris Smith <42@chrisryansmith.com>
2017-05-22 08:47:41 -0600

    add spec for level_of_interest grouping

--- spec/features/analysis/user_can_visit_dashboard_spec.rb
+++ spec/features/analysis/user_can_visit_dashboard_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
 RSpec.feature "User can view a dashboard" do
+  before {
+    create(:job, level_of_interest: 1)
+    create(:job, level_of_interest: 1)
+    create(:job, level_of_interest: 1)
+    create(:job, level_of_interest: 2)
+    create(:job, level_of_interest: 2)
+    create(:job, level_of_interest: 3)
+  }
+
   scenario "a user can visit /dashboard" do
     visit ('/dashboard')
     expect(page).to have_content "Jobs Dashboard"
+  end
+
+  scenario "a user can see a count of jobs by level_of_interest" do
+    visit ('/dashboard')
+    expect(page).to have_content "Count by level of interest"
+    expect(page).to have_css('.count_level_of_interest_1', :text => "3")
+    expect(page).to have_css('.count_level_of_interest_2', :text => "2")
+    expect(page).to have_css('.count_level_of_interest_3', :text => "1")
   end
 end
