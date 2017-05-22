3b1841ba05ac9998596af28573507d709f01bbdf
Chris Smith <42@chrisryansmith.com>
2017-05-21 18:03:37 -0600

    add validations to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :position, presence: true
+  validates :email, presence: true
+
   belongs_to :company
 
   def full_name
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,18 +3,42 @@ require 'rails_helper'
 RSpec.describe Contact, type: :model do
   describe "validations" do
     context "invalid attributes" do
+      it "is invalid without a first_name" do
+        contact = build(:contact, first_name: '')
+        expect(contact).to be_invalid
+      end
 
+      it "is invalid without a last_name" do
+        contact = build(:contact, last_name: '')
+        expect(contact).to be_invalid
+      end
+
+      it "is invalid without a position" do
+        contact = build(:contact, position: '')
+        expect(contact).to be_invalid
+      end
+
+      it "is invalid without a email" do
+        contact = build(:contact, email: '')
+        expect(contact).to be_invalid
+      end
     end
-    context "valid attributes" do
 
+    context "valid attributes" do
+      it "is valid with all the attributes included" do
+        contact = create(:contact)
+        expect(contact).to be_valid
+      end
     end
   end
+
   describe "relationships" do
     it "belongs to a company" do
       contact = create(:contact)
       expect(contact).to respond_to(:company)
     end
   end
+
   describe ".full_name" do
     it "returns the first and last name" do
       contact = create(:contact)
